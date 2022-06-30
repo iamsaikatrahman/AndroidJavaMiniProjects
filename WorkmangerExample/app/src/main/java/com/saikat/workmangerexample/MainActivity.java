bb8ea5184e9542d3263e01053d794c8f428a73ea
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2022-06-30 17:21:15 +0600

    OneTimeWorkRequest

--- WorkmangerExample/app/src/main/java/com/saikat/workmangerexample/MainActivity.java
+++ WorkmangerExample/app/src/main/java/com/saikat/workmangerexample/MainActivity.java
@@ -1,14 +1,49 @@
 package com.saikat.workmangerexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkInfo;
+import androidx.work.WorkManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btndowork, btnCancel;
+    private TextView tvStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btndowork = findViewById(R.id.btnDoWork);
+        tvStatus = findViewById(R.id.tvStatus);
+        btnCancel = findViewById(R.id.btnCancel);
+
+        //======================== OneTimeWorkRequest =============================//
+        OneTimeWorkRequest request = new OneTimeWorkRequest.Builder(MyWorker.class).build();
+
+        btndowork.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                WorkManager.getInstance(MainActivity.this).enqueue(request);
+
+            }
+        });
+
+        WorkManager.getInstance(this).getWorkInfoByIdLiveData(request.getId())
+                .observe(this, new Observer<WorkInfo>() {
+                    @Override
+                    public void onChanged(WorkInfo workInfo) {
+                        String status = workInfo.getState().name();
+                        tvStatus.setText(status + "\n");
+                    }
+                });
+
+        //======================== OneTimeWorkRequest =============================//
     }
 }
---
+++ WorkmangerExample/app/src/main/java/com/saikat/workmangerexample/MyWorker.java
@@ -0,0 +1,42 @@
+package com.saikat.workmangerexample;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.work.Data;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class MyWorker extends Worker {
+    public MyWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+//        Data retrievedData = getInputData();
+//        String desc = retrievedData.getString("input data");
+        displayNotification("Demo Task", "This is demo task");
+//        Data outputData = new Data.Builder().putString("output_data", "This is output data").build();
+        return Result.success();
+    }
+
+    private void displayNotification(String task, String desc) {
+        NotificationManager manager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("saikat", "workManager", NotificationManager.IMPORTANCE_DEFAULT);
+            manager.createNotificationChannel(channel);
+        }
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), "example")
+                .setContentTitle(task)
+                .setContentText(desc)
+                .setSmallIcon(R.mipmap.ic_launcher);
+
+        manager.notify(1, builder.build());
+    }
+}
