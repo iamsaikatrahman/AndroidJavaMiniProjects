ee6bcec9d09d696136d53131c1017a34026a9d3b
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2022-07-05 01:21:01 +0600

    add ArticleRepo in repository folder

--- NewsAppWithMVVMPattern/app/src/main/java/com/saikat/newsappwithmvvmpattern/api/ApiRequestInterface.java
+++ NewsAppWithMVVMPattern/app/src/main/java/com/saikat/newsappwithmvvmpattern/api/ApiRequestInterface.java
@@ -4,11 +4,10 @@ import static com.saikat.newsappwithmvvmpattern.constant.NewsAppConstant.API_KEY
 
 import com.saikat.newsappwithmvvmpattern.response.MainResponse;
 
-import java.util.List;
-
+import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface ApiRequestInterface {
     @GET("top-headlines?country=us&category=business&apiKey=" + API_KEY)
-    List<MainResponse> getTopHeadline();
+    Call<MainResponse> getTopHeadline();
 }
---
+++ NewsAppWithMVVMPattern/app/src/main/java/com/saikat/newsappwithmvvmpattern/repository/ArticleRepo.java
@@ -0,0 +1,38 @@
+package com.saikat.newsappwithmvvmpattern.repository;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.saikat.newsappwithmvvmpattern.api.ApiRequestInterface;
+import com.saikat.newsappwithmvvmpattern.api.RetrofitClient;
+import com.saikat.newsappwithmvvmpattern.response.MainResponse;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class ArticleRepo {
+    private final ApiRequestInterface apiRequestInterface;
+
+    public ArticleRepo() {
+        apiRequestInterface = RetrofitClient.getRetrofit().create(ApiRequestInterface.class);
+    }
+
+    public LiveData<MainResponse> getTopHeadlineUs() {
+        final MutableLiveData<MainResponse> data = new MutableLiveData<>();
+        apiRequestInterface.getTopHeadline().enqueue(new Callback<MainResponse>() {
+            @Override
+            public void onResponse(Call<MainResponse> call, Response<MainResponse> response) {
+                if (response.body() != null) {
+                    data.setValue(response.body());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<MainResponse> call, Throwable t) {
+                data.setValue(null);
+            }
+        });
+        return data;
+    }
+}
