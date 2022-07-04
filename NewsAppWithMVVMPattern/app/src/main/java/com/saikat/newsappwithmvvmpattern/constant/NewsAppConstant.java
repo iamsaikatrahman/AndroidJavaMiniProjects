df22bd4d9a15257afbd6b0f7d130d65918944140
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2022-07-04 08:43:13 +0600

    NewsAppWithMVVMPattern

---
+++ NewsAppWithMVVMPattern/app/src/main/java/com/saikat/newsappwithmvvmpattern/constant/NewsAppConstant.java
@@ -0,0 +1,9 @@
+package com.saikat.newsappwithmvvmpattern.constant;
+
+public class NewsAppConstant {
+
+    public static final String BASE_URL = "https://newsapi.org/v2/";
+    public static final String API_KEY = "ffbacf494acd4c19a85634642cf9914c";
+
+
+}
---
+++ NewsAppWithMVVMPattern/app/src/main/java/com/saikat/newsappwithmvvmpattern/ui/MainActivity.java
@@ -0,0 +1,17 @@
+package com.saikat.newsappwithmvvmpattern.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.saikat.newsappwithmvvmpattern.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //
+    }
+}
