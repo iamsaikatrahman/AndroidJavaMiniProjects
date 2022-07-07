5751a5bb723d84f3f9281438970dd88b225423d2
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2022-07-07 08:52:47 +0600

    update MainActivity"

--- NewsAppWithMVVMPattern/app/src/main/java/com/saikat/newsappwithmvvmpattern/ui/MainActivity.java
+++ NewsAppWithMVVMPattern/app/src/main/java/com/saikat/newsappwithmvvmpattern/ui/MainActivity.java
@@ -10,8 +10,10 @@ import android.view.View;
 import android.widget.ProgressBar;
 
 import com.saikat.newsappwithmvvmpattern.R;
+import com.saikat.newsappwithmvvmpattern.adapters.TopArticleAdapter;
 import com.saikat.newsappwithmvvmpattern.adapters.TopHeadlineAdapter;
 import com.saikat.newsappwithmvvmpattern.models.Article;
+import com.saikat.newsappwithmvvmpattern.models.TopArticle;
 import com.saikat.newsappwithmvvmpattern.viewmodel.MainViewModel;
 
 import java.util.ArrayList;
@@ -19,10 +21,12 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    RecyclerView recyclerView;
+    RecyclerView recyclerView, recyclerView2;
     List<Article> articleList;
+    List<TopArticle> topArticleList;
     MainViewModel mainViewModel;
     TopHeadlineAdapter adapter;
+    TopArticleAdapter adapter2;
     ProgressBar progressBar;
 
     @Override
@@ -32,6 +36,19 @@ public class MainActivity extends AppCompatActivity {
         progressBar = findViewById(R.id.loadingBar);
         initialize();
         getNews();
+
+        getTopArticleNews();
+    }
+
+    private void getTopArticleNews() {
+        mainViewModel.getTopArticle().observe(this, mainResponse -> {
+            if(mainResponse != null && mainResponse.getArticles() != null && !mainResponse.getArticles().isEmpty()){
+                List<TopArticle> article = mainResponse.getArticles();
+                topArticleList.addAll(article);
+                adapter2.notifyDataSetChanged();
+                progressBar.setVisibility(View.GONE);
+            }
+        });
     }
 
     private void getNews() {
@@ -50,9 +67,17 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false));
         recyclerView.setHasFixedSize(true);
 
+        recyclerView2 = findViewById(R.id.rec2);
+        recyclerView2.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+        recyclerView2.setHasFixedSize(true);
+
         articleList = new ArrayList<>();
         adapter = new TopHeadlineAdapter(articleList, this);
         recyclerView.setAdapter(adapter);
+
+        topArticleList = new ArrayList<>();
+        adapter2 = new TopArticleAdapter(topArticleList, this);
+        recyclerView2.setAdapter(adapter2);
 
 //        mainViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
         mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
