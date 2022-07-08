f1587a6ccc97e69d54ec05d486d47bb1c732b97f
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2022-07-08 21:13:22 +0600

    layout activity detailed design done

---
+++ NewsAppWithMVVMPattern/app/src/main/java/com/saikat/newsappwithmvvmpattern/ui/DetailedActivity.java
@@ -0,0 +1,14 @@
+package com.saikat.newsappwithmvvmpattern.ui;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.saikat.newsappwithmvvmpattern.R;
+
+public class DetailedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed);
+    }
+
+}
