2d2bd7f46b8bd96aafd3039a1a3a01ee256523ba
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2022-07-19 07:00:08 +0600

    create model data and util

---
+++ ContactRoom/app/src/main/java/com/saikat/contactroom/model/Contact.java
@@ -0,0 +1,50 @@
+package com.saikat.contactroom.model;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "contact_table")
+public class Contact {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name="id")
+    private int id;
+    @ColumnInfo(name="name")
+    private String name;
+    @ColumnInfo(name="occupation")
+    private String occupation;
+
+
+    public Contact() {
+    }
+
+    public Contact(int id, String name, String occupation) {
+        this.id = id;
+        this.name = name;
+        this.occupation = occupation;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+}
