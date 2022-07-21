17c26797fcde808086109e02d325fc187f557edf
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2022-07-21 21:45:14 +0600

    contact information add in database

--- ContactRoom/app/src/main/java/com/saikat/contactroom/NewContact.java
+++ ContactRoom/app/src/main/java/com/saikat/contactroom/NewContact.java
@@ -1,15 +1,23 @@
 package com.saikat.contactroom;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.saikat.contactroom.model.Contact;
+import com.saikat.contactroom.model.ContactViewModel;
 
 public class NewContact extends AppCompatActivity {
     private EditText enterName;
     private EditText enterOccupation;
     private Button saveInfoButton;
+    private ContactViewModel contactViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +27,18 @@ public class NewContact extends AppCompatActivity {
         enterName = findViewById(R.id.enter_name);
         enterOccupation = findViewById(R.id.enter_occupation);
         saveInfoButton = findViewById(R.id.save_button);
+
+        contactViewModel = new ViewModelProvider.AndroidViewModelFactory(NewContact.this
+                .getApplication())
+                .create(ContactViewModel.class);
+
+        saveInfoButton.setOnClickListener(view -> {
+            if (!TextUtils.isEmpty(enterName.getText()) && !TextUtils.isEmpty(enterOccupation.getText())) {
+                Contact contact = new Contact(enterName.getText().toString(), enterOccupation.getText().toString());
+               ContactViewModel.insert(contact);
+            } else {
+                Toast.makeText(this, R.string.empty, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
