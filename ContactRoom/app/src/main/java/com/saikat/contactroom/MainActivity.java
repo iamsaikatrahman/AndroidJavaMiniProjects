160f3fa34bb84e34d82eb44fa5076abd252ec2af
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2022-07-22 01:25:04 +0600

    Adding Custom Contacts Via the Contact Activity

--- ContactRoom/app/src/main/java/com/saikat/contactroom/MainActivity.java
+++ ContactRoom/app/src/main/java/com/saikat/contactroom/MainActivity.java
@@ -1,19 +1,24 @@
 package com.saikat.contactroom;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.saikat.contactroom.model.Contact;
 import com.saikat.contactroom.model.ContactViewModel;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int NEW_CONTACT_ACTIVITY_REQUEST_CODE = 1;
     private ContactViewModel contactViewModel;
     private TextView textView;
 
@@ -37,5 +42,23 @@ public class MainActivity extends AppCompatActivity {
                 textView.setText(builder.toString());
             }
         });
+
+        FloatingActionButton fab = findViewById(R.id.add_contact_fab);
+        fab.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, NewContact.class);
+            startActivityForResult(intent, NEW_CONTACT_ACTIVITY_REQUEST_CODE);
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == NEW_CONTACT_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK){
+            String name = data.getStringExtra(NewContact.NAME_REPLY);
+            String occupation = data.getStringExtra(NewContact.OCCUPATION_REPLY);
+            Contact contact = new Contact(name, occupation);
+            ContactViewModel.insert(contact);
+            //Log.d("MainTAG", "onActivityResult: Name: "+ data.getStringExtra(NewContact.NAME_REPLY) + " " + "Occupation: "+ data.getStringExtra(NewContact.OCCUPATION_REPLY));
+        }
     }
 }
--- ContactRoom/app/src/main/java/com/saikat/contactroom/NewContact.java
+++ ContactRoom/app/src/main/java/com/saikat/contactroom/NewContact.java
@@ -3,6 +3,7 @@ package com.saikat.contactroom;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -14,6 +15,8 @@ import com.saikat.contactroom.model.Contact;
 import com.saikat.contactroom.model.ContactViewModel;
 
 public class NewContact extends AppCompatActivity {
+    public static final String NAME_REPLY = "name_reply";
+    public static final String OCCUPATION_REPLY = "occupation_reply";
     private EditText enterName;
     private EditText enterOccupation;
     private Button saveInfoButton;
@@ -33,12 +36,20 @@ public class NewContact extends AppCompatActivity {
                 .create(ContactViewModel.class);
 
         saveInfoButton.setOnClickListener(view -> {
+            Intent replyIntent = new Intent();
             if (!TextUtils.isEmpty(enterName.getText()) && !TextUtils.isEmpty(enterOccupation.getText())) {
+                String name = enterName.getText().toString();
+                String occupation = enterOccupation.getText().toString();
                 Contact contact = new Contact(enterName.getText().toString(), enterOccupation.getText().toString());
-               ContactViewModel.insert(contact);
+                //ContactViewModel.insert(contact);
+                replyIntent.putExtra(NAME_REPLY, name);
+                replyIntent.putExtra(OCCUPATION_REPLY, occupation);
+                setResult(RESULT_OK, replyIntent);
             } else {
-                Toast.makeText(this, R.string.empty, Toast.LENGTH_SHORT).show();
+                //Toast.makeText(this, R.string.empty, Toast.LENGTH_SHORT).show();
+                setResult(RESULT_CANCELED, replyIntent);
             }
+            finish();
         });
     }
 }
