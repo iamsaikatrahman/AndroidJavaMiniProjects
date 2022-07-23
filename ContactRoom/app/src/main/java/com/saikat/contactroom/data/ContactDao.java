9bcd566b95ef7b1b16fbbfdb31ee166f454e3048
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2022-07-23 11:30:20 +0600

    Setting Up the Full CRUD Operations

--- ContactRoom/app/src/main/java/com/saikat/contactroom/data/ContactDao.java
+++ ContactRoom/app/src/main/java/com/saikat/contactroom/data/ContactDao.java
@@ -2,9 +2,11 @@ package com.saikat.contactroom.data;
 
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import com.saikat.contactroom.model.Contact;
 
@@ -12,7 +14,7 @@ import java.util.List;
 
 @Dao
 public interface ContactDao {
-
+    //CRUD
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     void insert(Contact contact);
 
@@ -21,4 +23,13 @@ public interface ContactDao {
 
     @Query("SELECT * FROM contact_table ORDER BY name ASC")
     LiveData<List<Contact>> getAllContacts();
+
+    @Query("SELECT * FROM contact_table WHERE contact_table.id == :id")
+    LiveData<Contact> get(int id);
+
+    @Update
+    void update(Contact contact);
+
+    @Delete
+    void delete(Contact contact);
 }
