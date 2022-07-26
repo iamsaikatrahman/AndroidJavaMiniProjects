c0ab989c2c34a057e623b427c1eff24964ea9185
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2022-07-27 01:35:16 +0600

    Creating the Task ViewModel

--- Todoister/app/src/main/java/com/saikat/todoister/MainActivity.java
+++ Todoister/app/src/main/java/com/saikat/todoister/MainActivity.java
@@ -1,14 +1,28 @@
 package com.saikat.todoister;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+
+import android.app.Application;
 import android.os.Bundle;
 
+import com.saikat.todoister.model.TaskViewModel;
+
 public class MainActivity extends AppCompatActivity {
+    private TaskViewModel taskViewModel;
+    private RecyclerView recyclerView;
+    //    private RecyclerViewAdapter recyclerViewAdapter;
+    BottomSheetFragment bottomSheetFragment;
+//    private ShareViewModel shareViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
 }
